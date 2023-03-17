fcdaef6034fd7f749d085fa60094ba2f5367a8f1
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-03-16 23:51:01 -0700

    update

---
+++ build/precache-manifest.864c3373cd6e835ed1494ae42c3740a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0e86391877dc0dd6086",
+    "url": "/arjdad.com/static/css/main.7916cfbd.chunk.css"
+  },
+  {
+    "revision": "c0e86391877dc0dd6086",
+    "url": "/arjdad.com/static/js/main.e85dcad0.chunk.js"
+  },
+  {
+    "revision": "ebd4e8582a6aad352709",
+    "url": "/arjdad.com/static/js/runtime~main.0e4171f7.js"
+  },
+  {
+    "revision": "65160dec713410ac8699",
+    "url": "/arjdad.com/static/js/2.79b99724.chunk.js"
+  },
+  {
+    "revision": "64fe8b082aa1e7cabcc56f419504d8ad",
+    "url": "/arjdad.com/index.html"
+  }
+];
